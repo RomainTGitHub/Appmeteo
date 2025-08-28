432d2df3312287a2296fc96931ec7c5630da4e53
RomainTGitHub <romain.turlotte@icloud.com>
2025-08-28 17:40:20 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Appmeteo/', // Réajustez ce chemin pour GitHub Pages
+  base: '/Appmeteo/', 
 })
